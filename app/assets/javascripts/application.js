7eaabc998f206a22f9b5c8d67a12d1d2557b0d6f
Timgriffin1 <timgriffin1@gmail.com>
2019-12-03 01:03:47 +0000

    bundle tweets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
-//= require bootstrap
+//= require tether
+//= require bootstrap-sprockets
+//= require_tree .
+
--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,4 @@ module Ascendant
     # the framework and any gems in your application.
   end
 end
+
